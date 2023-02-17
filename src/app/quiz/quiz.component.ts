33103092b86ee88611c539dcf1f132d55dc85cce
jkkoloma <kolomajeremi60@gmail.com>
2023-02-17 15:18:23 +0000

    Dashboard presque OK

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Quiz } from '../models/quiz';
+import { QuizService } from '../services/quiz.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+
+  quizList !: Quiz[]
+  quizHost!: any;
+  
+  constructor(
+    private quizService: QuizService,
+  ){}
+
+
+  ngOnInit() {
+
+    this.quizHost = this.quizService.quizHost;
+    
+    this.TogetQuizList()
+
+  }
+
+
+
+
+    // Une fonction pour afficher la liste des Quiz
+    TogetQuizList() {
+      this.quizService.getQuiz().subscribe(
+        (data) => {
+          this.quizList = data;
+        }
+      )
+    }
 
 }
